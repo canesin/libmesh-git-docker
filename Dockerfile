62b0e77c4565c1c3f54394c2dc5f2b14138b3529
Fábio César Canesin <fabio.canesin@gmail.com>
2015-10-10 22:14:27 -0300

    one more try

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ ENV DEBIAN_FRONTEND noninteractive
 RUN apt-get update && apt-get -y upgrade
 RUN apt-get install -y \
     build-essential \
+    python-setuptools \
     gfortran \
     git-core
 
--- petsc.sh
+++ petsc.sh
@@ -5,7 +5,14 @@ cd /tmp/build/
 tar -xzf petsc.tar.gz
 rm petsc.tar.gz
 cd `ls | grep petsc`
-./configure PETSC_DIR=/opt/petsc PETSC_ARCH=dorcker-gnu-opt --with-blas-lapack-dir=/opt/OpenBLAS/lib --with-debugging=0 COPTFLAGS='-O3 -march=core-avx2 -mtune=core-avx2' CXXOPTFLAGS='-O3 -march=core-avx2 -mtune=core-avx2' FOPTFLAGS='-O3 -march=core-avx2 -mtune=core-avx2' --download-mpich
+./configure PETSC_DIR=/opt/petsc \
+            PETSC_ARCH=dorcker-gnu-opt \
+            --with-blas-lapack-dir=/opt/OpenBLAS/lib \
+            --with-debugging=0 COPTFLAGS='-O3 -march=core-avx2 -mtune=core-avx2' \
+            CXXOPTFLAGS='-O3 -march=core-avx2 -mtune=core-avx2' \
+            FOPTFLAGS='-O3 -march=core-avx2 -mtune=core-avx2' \
+            --download-mpich
+
 make PETSC_DIR=/opt/petsc PETSC_ARCH=dorcker-gnu-opt
 # Clean
 cd /
