02708faad3b7e358a92d78e779a1559dd09bc7a4
Fábio César Canesin <fabio.canesin@gmail.com>
2016-02-26 19:03:42 -0300

    following github and email talks: disable openmp pthreads and mantainer_mode

--- libmesh.sh
+++ libmesh.sh
@@ -13,12 +13,19 @@ export SLEPC_DIR=/opt/slepc
 # Build version for opmitzed code, profile with gprof and full debug for dev
 ./configure --with-methods="opt pro dbg" \
             --prefix=/opt/libmesh \
+            --with-metis=PETSc \
+            --enable-tbb \
+            --enable-ifem \
+            --enable-cxx11 \
             --enable-silent-rules \
             --enable-unique-ptr \
             --enable-unique-id \
             --disable-warnings \
             --disable-timestamps \
-            --disable-fortran
+            --disable-fortran \
+            --disable-pthreads \
+            --disable-openmp \
+            --disable-maintainer-mode
 
 make -j 4
 make install -j 4
@@ -26,6 +33,7 @@ make installcheck METHODS=opt
 
 # Add LIBMESH_DIR to env variables
 echo "export LIBMESH_DIR=/opt/libmesh" >> $HOME/.bashrc
+echo "export PATH=$LIBMESH_DIR/bin:$PATH" >> $HOME/.bashrc
 source $HOME/.bashrc
 
 # Clean files
